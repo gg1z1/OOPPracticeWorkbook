479a8eb8195ab1c6caa21db901c05e53fa1428d0
Tumanov Artem <arti.msk2011@gmail.com>
2025-08-10 01:41:39 +0300

    Практика ООП. *Исключения #3. Свое исключение - задание необязательное

--- src/main/java/com/stepup/mains/MainException.java
+++ src/main/java/com/stepup/mains/MainException.java
@@ -3,18 +3,33 @@ package com.stepup.mains;
 import com.stepup.exceptions.OperationAttemptException;
 
 public class MainException {
+
     public static void main(String[] args) {
+        SquareRootCalculator root = new SquareRootCalculator();
+
         try {
-            performOperation();
+            // Успешный случай
+            double result = root.calculateSquareRoot(16);
+            System.out.println("Квадратный корень: " + result);
+
+            result = root.calculateSquareRoot(-9);
+            System.out.println("Квадратный корень: " + result);
+
         } catch (OperationAttemptException e) {
-            System.err.println("Произошла ошибка: " + e.getMessage());
+            System.err.println("Ошибка при вычислении корня: " + e.getMessage());
         }
     }
+}
 
-    public static void performOperation() throws OperationAttemptException {
-        // Имитация условия, при котором возникает исключение
-        if (true) {
-            throw new OperationAttemptException("Операция не может быть выполнена");
-        }
+class SquareRootCalculator {
+    /**
+     * Вычисляет квадратный корень числа
+     * @param number число, из которого извлекается корень
+     * @return квадратный корень числа
+     * @throws OperationAttemptException если число отрицательное
+     */
+    public double calculateSquareRoot(double number) throws OperationAttemptException {
+        if (number < 0) throw new OperationAttemptException("Невозможно извлечь корень из отрицательного числа: " + number);
+        return Math.sqrt(number);
     }
 }
